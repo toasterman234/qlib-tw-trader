143a493be85aca8a648be8fe693f6c339a7e3a4b
toasterman234 <bencharney@yahoo.com>
2026-04-18 00:48:01 -0500

    Make default factor families market-aware

--- src/repositories/factors/__init__.py
+++ src/repositories/factors/__init__.py
@@ -1,30 +1,21 @@
-"""
-因子定義模組
-
-匯出所有因子定義，包含：
-- Alpha158 純 K 線因子 (~109 個)
-- 台股籌碼因子 (~130 個)
-- 交互因子 (~55 個)
-- 增強因子 (~37 個)
-"""
+"""Factor definition exports."""
 
 from src.repositories.factors.alpha158 import ALPHA158_FACTORS
 from src.repositories.factors.enhanced import ENHANCED_FACTORS
 from src.repositories.factors.interaction import INTERACTION_FACTORS
 from src.repositories.factors.taiwan_chips import TAIWAN_CHIPS_FACTORS
+from src.shared.market import market_is_us
 
-# 匯出所有因子
-ALL_FACTORS = (
-    ALPHA158_FACTORS
-    + TAIWAN_CHIPS_FACTORS
-    + INTERACTION_FACTORS
-    + ENHANCED_FACTORS
-)
+GENERIC_FACTORS = ALPHA158_FACTORS + INTERACTION_FACTORS + ENHANCED_FACTORS
+TW_FACTORS = GENERIC_FACTORS + TAIWAN_CHIPS_FACTORS
+ALL_FACTORS = GENERIC_FACTORS if market_is_us() else TW_FACTORS
 
 __all__ = [
     "ALPHA158_FACTORS",
     "TAIWAN_CHIPS_FACTORS",
     "INTERACTION_FACTORS",
     "ENHANCED_FACTORS",
+    "GENERIC_FACTORS",
+    "TW_FACTORS",
     "ALL_FACTORS",
 ]
